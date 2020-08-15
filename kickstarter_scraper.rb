8b93398aee1b41c4d8eb198bcf61e3d8c14348fb
Pablo Menz <p.menz.r@gmail.com>
2020-08-15 05:17:19 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,3 +1,21 @@
+
+def create_project_hash
+  projects = {}
+ 
+  kickstarter.css("li.project.grid_4").each do |project|
+    title = project.css("h2.bbcard_name strong a").text
+    projects[title.to_sym] = {}
+  end
+ 
+  # return the projects hash
+  projects
+end
+That's better. You'll notice that we're converting the title into a symbol using the to_sym method. Remember that symbols make better hash keys than strings.
+
+Finally, it's just a matter of grabbing each of the data points using the selectors we've already figured out, and adding them to each project's hash. So, our complete code will look something like this:
+
+# file: kickstarter_scraper.rb
+ 
 require 'nokogiri'
 require 'pry'
  
@@ -23,4 +41,7 @@ def create_project_hash
       :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
     }
   end
-  
+ 
+  # return the projects hash
+  projects
+end

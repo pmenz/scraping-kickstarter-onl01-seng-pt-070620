986b6cb41661d0b64f8a899c078d64b15dcd48c8
Pablo Menz <p.menz.r@gmail.com>
2020-08-15 05:11:12 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,20 +1,13 @@
-
-
 require "nokogiri"
+require "pry"
 
-def create_project_hash
- 
 
- 
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
   binding.pry
 end
 
-html = File.read('fixtures/kickstarter.html')
- 
-kickstarter = Nokogiri::HTML(html)
 
 
 
